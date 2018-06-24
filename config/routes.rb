ffeae7db9e141ee6dca01e43db907882e1180f12
rschmelter <rschmelter33@gmail.com>
2018-06-24 11:53:16 -0400

    Add nested routes for users/projects users/materials users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
+  resources :users, only: [:show] do 
+    resources :projects
+  end
+
+  resources :users, only: [:show] do
+    resources :materials
+  end
+
+  resources :projects, only: [:index, :show]
+    
+
+
+
+  resources :users
+
+  
+
+
 end
