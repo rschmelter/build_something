9d651b9770324f1cf8555791492308fe469bdf34
rschmelter <rschmelter33@gmail.com>
2018-06-25 19:31:35 -0400

    add before actions to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+  before_action :require_logged_in, except: [:new, :create, :home]
  
   def logged_in?
     !!current_user 
@@ -9,7 +11,7 @@ class ApplicationController < ActionController::Base
   private 
 
   def require_logged_in 
-    redirect_to root path unless logged_in? 
+    redirect_to root_path unless logged_in? 
   end 
 
   def current_user 
