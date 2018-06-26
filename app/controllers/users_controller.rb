c8b6c023b6b4b4d1eb7c78311968173e64f91dce
rschmelter <rschmelter33@gmail.com>
2018-06-25 20:16:41 -0400

    fix signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController 
-
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
   def index 
     @users = User.all
 
@@ -25,6 +26,10 @@ class UsersController < ApplicationController
   end
 
   private 
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params 
     params.require(:user).permit(:name, :password)
