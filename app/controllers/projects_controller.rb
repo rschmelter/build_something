8f4a9395225905340e07b29be5588cde30d6999c
rschmelter <rschmelter33@gmail.com>
2018-06-27 19:39:11 -0400

    Projects Controller add new create actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,39 @@
 class ProjectsController < ApplicationController 
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  
+  def index 
+
+  end 
+  
   def new 
     @project = Project.new(user_id: params[:user_id])
 
 
   end 
+
+  def show 
+  
+
+  end
+
+  def create 
+    @project = Project.create(project_params)
+
+  end
+
+  private 
+
+  def set_project 
+    @project = project.find(params[:id])
+
+  end
+
+  def project_params 
+    params.require(:project).permit(:name, :cost, :difficulty, :type, :user_id)
+
+  end
+
 
 
 
