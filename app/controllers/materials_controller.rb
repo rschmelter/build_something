21081bc2c5fd330ca6c5983b087405d4fed956b5
rschmelter <rschmelter33@gmail.com>
2018-07-16 19:18:01 -0400

    add Material Class Mehods

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,7 +1,10 @@
 class MaterialsController < ApplicationController 
 
 
-
+def index 
+  tools = Material.tool
+  raw_materials = Material.raw_material
+end 
 
 
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -3,4 +3,13 @@ class Material < ActiveRecord::Base
   has_many :projects, :through => :project_materials
 
   
+
+  def self.tool 
+    where(tool: true)
+  end
+
+  def self.raw_material
+    where(tool: false)
+  end 
+
 end
