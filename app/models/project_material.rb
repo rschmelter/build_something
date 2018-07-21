9d0d38a5e3b4b3f634e82e8fd414a00201acabde
rschmelter <rschmelter33@gmail.com>
2018-07-21 11:13:07 -0400

    class methods in projectmaterial

--- app/models/project_material.rb
+++ app/models/project_material.rb
@@ -3,4 +3,13 @@ class ProjectMaterial < ActiveRecord::Base
   belongs_to :material
 
 
+
+def self.material_id_count 
+  self.group(:material_id).count
+end 
+
+def self.most_common_material_id
+  material_id_count.first[0]
+end 
+
 end
