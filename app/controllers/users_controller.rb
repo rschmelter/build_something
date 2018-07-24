9848b390795b999746e204cef416a59bc629ffae
rschmelter <rschmelter33@gmail.com>
2018-07-24 19:25:52 -0400

    add validation to user edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,10 @@ class UsersController < ApplicationController
     end
   end 
 
+  def edit 
+    redirect_to user_path(current_user) unless @user.id == current_user.id
+  end 
+
   def create 
     @user = User.new(user_params)
     if @user.save 
