5a52bd56ae9ac344ef8c590b9ec714dc2fc6aecc
rschmelter <rschmelter33@gmail.com>
2018-10-12 11:56:34 -0700

    add constructor function to project.js

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,8 +1,4 @@
-// $(document).ready(function() {
-//     $("H1").hover(function(){
-//         $(this).fadeOut(100);
-//     });
-// })
+
 
 $(function(){
     $("#load_projects").on("click", function(e){
@@ -17,3 +13,17 @@ $(function(){
         e.preventDefault();
     });
 });
+
+class Project{
+    constructor(projectJson) {
+        this.id = projectJson.id;
+        this.name = projectJson.name;
+        this.project_type = projectJson.project_type;
+    };
+
+    formatProject() {
+        return `${this.name} is a ${this.project_type} project`;
+    }
+};
+
+// grab attributes to append to the page.
