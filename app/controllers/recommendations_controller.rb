89ba72864aea95f6c3f255b3493061c359573f75
rschmelter <rschmelter33@gmail.com>
2018-10-20 12:40:34 -0700

    trying to get recommendation form to work

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,3 +1,20 @@
 class RecommendationsController < ApplicationController
 
+    def create 
+        @material = Material.find(params[:id])
+        @recommendation = @material.recommendations.build(recommendations_params)
+            if @recommendation.save
+                redirect_to @material
+            else
+                render "materials/show"
+            end
+    end
+
+
+private
+
+    def recommendations_params
+        params.require(:recommendation).permit(:name, :cost, :buy)
+    end
+
 end
