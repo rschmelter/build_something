29f17b695feb1079cb0108f99cb64bb05c7b0cf1
rschmelter <rschmelter33@gmail.com>
2018-10-22 15:28:57 -0700

    Recommendations form working

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,13 +1,16 @@
 class RecommendationsController < ApplicationController
 
     def create 
-        @material = Material.find(params[:id])
+        
+       @material = Material.find(params[:material_id])
         @recommendation = @material.recommendations.build(recommendations_params)
             if @recommendation.save
+                
                 redirect_to @material
             else
                 render "materials/show"
             end
+        
     end
 
 
