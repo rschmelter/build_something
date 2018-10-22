168535849e9f4e8d951738594e25b6384fc508e6
rschmelter <rschmelter33@gmail.com>
2018-10-22 15:56:15 -0700

    create class constructor in recommendation.js

---
+++ app/assets/javascripts/recommendation.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+class Recommendation {
+    constructor(recommendationJson){
+
+    this.id = recommendationJson.id;
+    this.name = recommendationJson.name;
+    this.cost = recommendationJson.cost;
+    this.buy = recommendationJson.buy;
+    };
+
+}
